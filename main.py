4f618b909838521d4b5d1b5f9fd51b833f9d7653
Rodrigo silva <rodsilvasoul@gmail.com>
2021-11-26 23:57:51 -0400

    feat: phones model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_cors import CORS
 
 from routes.users import *
+from routes.emails import *
 
 app = Flask(__name__)
 
@@ -14,3 +15,9 @@ api.add_resource(CreateUserRoute, '/users')
 api.add_resource(GetUserByIdRoute, '/users/<int:id>')
 api.add_resource(UpdateUserByIdRoute, '/users/<int:id>')
 api.add_resource(DeleteUserByIdRoute, '/users/<int:id>')
+
+# emails routes
+
+api.add_resource(CreateEmailRoute, '/emails')
+api.add_resource(UpdateEmailByIdRoute, '/emails/<int:id>')
+api.add_resource(DeleteEmailByIdRoute, '/emails/<int:id>')
